6554d313f4dd2ab392cb4ee283a98ffa3c30ed02
Alexander F Rødseth <rodseth@gmail.com>
2016-04-05 15:41:05 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package echoperm
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/xyproto/permissions2"
+	"github.com/xyproto/pinterface"
+)
+
+// Set up a middleware handler for Echo
+func Middleware(redisHostname, redisPassword, denyMessage string) (echo.MiddlewareFunc, pinterface.IUserState, error) {
+	userstate, err := permissions.NewUserStateWithPassword2(redisHostname, redisPassword)
+	if err != nil {
+		return nil, nil, err
+	}
+	perm := permissions.NewPermissions(userstate)
+
+	return echo.MiddlewareFunc(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return echo.HandlerFunc(func(c echo.Context) error {
+			// Check if the user has the right admin/user rights
+			if perm.Rejected(c.Response().(*standard.Response).ResponseWriter, c.Request().(*standard.Request).Request) {
+				// Deny the request
+				return echo.NewHTTPError(http.StatusForbidden, denyMessage)
+			}
+			// Continue the chain of middleware
+			return next(c)
+		})
+	}), userstate, nil
+}
